51eca95b465cb53dbef1564fbccdc1a232d98c0b
Lev <kulyklev12@gmail.com>
2019-01-31 16:13:56 +0200

    add dropping items with two or more isbns

--- parser_src/pipelines/filter_items_pipeline.py
+++ parser_src/pipelines/filter_items_pipeline.py
@@ -5,11 +5,28 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 from scrapy.exceptions import DropItem
+from items.book_item import BookItem
+# from parser_src.items.book_item import BookItem
 
 
 class FilterItemsPipeline(object):
     def process_item(self, item, spider):
-        if item['isbn'] is None:
+        if not self.has_isbn(item):
             raise DropItem("Missing ISBN in %s" % item)
+        elif self.has_multiple_isbns(item):
+            raise DropItem("Two or more ISBNs in %s" % item)
         else:
             return item
+
+    def has_isbn(self, item: BookItem) -> bool:
+        if item['isbn'] is None:
+            return False
+        else:
+            return True
+
+    def has_multiple_isbns(self, item) -> bool:
+        if "," not in item['isbn']:
+            return False
+        else:
+            return True
+
