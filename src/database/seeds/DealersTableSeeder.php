089de180a3e60e8bcec70d570f774889187a033a
Lev <kulyklev12@gmail.com>
2019-02-22 19:38:04 +0200

    add seeders to categories and dealer tables

---
+++ src/database/seeds/DealersTableSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class DealersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('dealers')->insert([
+            'site_name' => 'bookclub.ua'
+        ]);
+    }
+}
