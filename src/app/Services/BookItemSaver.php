8372cbeeb4a677afdaee4cfd2caa40f20060addf
Lev <kulyklev12@gmail.com>
2019-03-06 14:32:49 +0200

    improve saving of parsed data

--- src/app/Services/BookItemSaver.php
+++ src/app/Services/BookItemSaver.php
@@ -11,6 +11,7 @@ namespace App\Services;
 
 use App\Models\Book;
 use App\Models\Category;
+use App\Models\Dealer;
 use App\Models\Offer;
 use App\Models\Price;
 use App\Models\Publisher;
@@ -26,16 +27,38 @@ class BookItemSaver
     {
         $decodedJsonData = json_decode($jsonBookItem);
 
-        $this->saveNewBook($decodedJsonData);
-        //TODO Implement making decision how to process data
+        switch ($decodedJsonData->data_type) {
+            case "bookItem":
+                $this->processBookItem($decodedJsonData);
+                break;
+
+            case "reparsedPrice":
+                $this->saveNewPrice($decodedJsonData);
+                break;
+
+            default:
+            // TODO Maybe add logging or messaging
+                break;
+        }
     }
 
-    public function saveNewBook($decodedJsonData)
+    public function processBookItem($decodedJsonData)
+    {
+        if (Book::where('isbn', $decodedJsonData->isbn)->exists()) {
+            $this->insertMissingValuesIntoBook($decodedJsonData);
+        } else {
+            $this->saveNewBook($decodedJsonData);
+        }
+    }
+
+    /**
+     * @param $decodedJsonData
+     */
+    public function saveNewBook($decodedJsonData): void
     {
         $publisher = $this->savePublisher($decodedJsonData->publisher);
         $book = $this->saveBook($decodedJsonData, $publisher->id);
         $newOffer = $this->saveOffer($decodedJsonData, $book);
-
         $this->savePrice($decodedJsonData, $newOffer);
     }
 
@@ -80,9 +103,11 @@ class BookItemSaver
      */
     public function saveOffer($decodedJsonData, Book $book): Offer
     {
+        $dealer = Dealer::where('site_name', $decodedJsonData->dealer_name)->first();
+
         $newOffer = new Offer();
         $newOffer->book_id = $book->id;
-        $newOffer->dealer_id = 1;
+        $newOffer->dealer_id = $dealer->id;
         $newOffer->link = $decodedJsonData->link;
         $newOffer->image = $decodedJsonData->image[0]->path;
         $newOffer->save();
@@ -102,9 +127,48 @@ class BookItemSaver
         $newPrice->save();
     }
 
-    public function insertMissingValuesToBook($decodedJsonData)
+    public function insertMissingValuesIntoBook($decodedJsonData): void
     {
+        $book = Book::where('isbn', $decodedJsonData->isbn)->first();
 
+        if ($book->original_name == null && $decodedJsonData->original_name != null) {
+            $book->original_name = $decodedJsonData->original_name;
+        }
+
+        if ($book->publishing_year == null && $decodedJsonData->publishing_year != null) {
+            $book->publishing_year = $decodedJsonData->publishing_year;
+        }
+
+        if ($book->weight == null && $decodedJsonData->weight != null) {
+            $book->weight = $decodedJsonData->weight;
+        }
+
+        if ($book->language == null && $decodedJsonData->language != null) {
+            $book->language = $decodedJsonData->language;
+        }
+
+        if ($book->original_language == null && $decodedJsonData->original_language != null) {
+            $book->original_language = $decodedJsonData->original_language;
+        }
+
+        if ($book->paperback == null && $decodedJsonData->paperback != null) {
+            $book->paperback = $decodedJsonData->paperback;
+        }
+
+        if ($book->product_dimensions == null && $decodedJsonData->product_dimensions != null) {
+            $book->product_dimensions = $decodedJsonData->product_dimensions;
+        }
+
+        $book->save();
+    }
+
+    protected function saveNewPrice($decodedJsonData)
+    {
+        $book = Book::where('isbn', $decodedJsonData->isbn)->first();
+        $dealer = Dealer::where('site_name', $decodedJsonData->dealer_name)->first();
+        $offer = $book->offers()->where('dealer_id', $dealer->id)->first();
+
+        $this->savePrice($decodedJsonData, $offer);
     }
 
 }
