1cc207ce08e0a031c614961add3d8278025fc8bd
Lev <kulyklev12@gmail.com>
2019-03-23 00:27:44 +0200

    add basic vue setup

--- src/resources/js/app.js
+++ src/resources/js/app.js
@@ -1,3 +1,5 @@
+import router from './router'
+import App from './components/App'
 
 /**
  * First we will load all of this project's JavaScript dependencies which
@@ -14,13 +16,13 @@ window.Vue = require('vue');
  * Vue components. It will recursively scan this directory for the Vue
  * components and automatically register them with their "basename".
  *
- * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
+ * Eg. ./components/App.vue -> <example-component></example-component>
  */
 
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+Vue.component('example-component', require('./components/App.vue').default);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
@@ -29,5 +31,7 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
  */
 
 const app = new Vue({
-    el: '#app'
+    el: '#app',
+    router,
+    render: h => h(App)
 });
---
+++ src/resources/js/components/App.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="container">
+        <h1>lalala</h1>
+        <router-view></router-view>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
