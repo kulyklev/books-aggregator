adb53316f3c1f491cfdcc0f74c1740eef37a9b92
Lev <kulyklev12@gmail.com>
2019-04-01 19:21:30 +0300

    change category resource structure

--- src/app/Http/Resources/Category.php
+++ src/app/Http/Resources/Category.php
@@ -18,16 +18,7 @@ class Category extends JsonResource
         return [
             'id' => $this->id,
             'name' => $this->name,
-            'links' => $this->when($this->isCategoriesShowRoute($request), new CategoryLinkCollection($this->categoryLinks))
+            'links' => $this->categoryLinks
         ];
-    }
-
-    /**
-     * @param  \Illuminate\Http\Request  $request
-     * @return bool
-     */
-    private function isCategoriesShowRoute(Request $request): bool
-    {
-        return $request->route()->getName() == 'categories.show';
     }
 }
