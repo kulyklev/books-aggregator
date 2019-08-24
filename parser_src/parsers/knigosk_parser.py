0ac0cfbbd6c5572a016128c1215cbc3ccd36bce6
Lev <kulyklev12@gmail.com>
2019-08-24 16:20:19 +0300

    add some basic implementation for parsing knigosklad

---
+++ parser_src/parsers/knigosk_parser.py
@@ -0,0 +1,107 @@
+from parsers.base_parser import BaseParser
+from items.book_item import BookItem
+from items.reparsed_book_item import ReparsedBookItem
+# from parser_src.items.book_item import BookItem
+# from parser_src.parsers.base_parser import BaseParser
+# from parser_src.items.reparsed_book_item import ReparsedBookItem
+
+
+class KnigoskParser(BaseParser):
+
+    def __init__(self):
+        pass
+
+    def parse_book_page(self, response):
+        book_item = BookItem()
+
+        book_item['name'] = self.parse_name(response)
+        book_item['original_name'] = self.parse_original_name(response)
+        book_item['author'] = self.parse_author(response)
+        book_item['price'] = self.parse_price(response)
+        book_item['currency'] = self.parse_currency(response)
+        book_item['language'] = self.parse_language(response)
+        book_item['original_language'] = self.parse_original_language(response)
+        book_item['paperback'] = self.parse_paperback(response)
+        book_item['product_dimensions'] = self.parse_product_dimensions(response)
+        book_item['publisher'] = self.parse_publisher(response)
+        book_item['publishing_year'] = self.parse_publishing_year(response)
+        book_item['isbn'] = self.parse_isbn(response)
+        book_item['link'] = response.url
+        book_item['image_urls'] = self.parse_image_urls(response)
+        book_item['weight'] = self.parse_weight(response)
+
+        return book_item
+
+    def reparse_book_page(self, response):
+        book_item = ReparsedBookItem()
+        book_item['price'] = self.parse_price(response)
+        book_item['currency'] = self.parse_currency(response)
+        book_item['isbn'] = self.parse_isbn(response)
+        return book_item
+
+    def parse_name(self, response):
+        name = response.xpath("//h1[@itemprop='name']/text()").extract_first()
+        return name
+
+    def parse_original_name(self, response):
+        # TODO I didn`t find this field yet
+        return None
+
+    def parse_author(self, response):
+        author = response.xpath("//div[@class='author-name']/a/text()").extract_first()
+        return author
+
+    def parse_price(self, response):
+        price = response.xpath("//div[contains(@class, 'product-shop')]//span[@class='regular-price']/span[@class='price']/text()").extract_first()
+        return price
+
+    def parse_currency(self, response):
+        # TODO Implement
+        currency = response.xpath("").extract_first()
+        return currency
+
+    def parse_language(self, response):
+        # TODO Implement
+        language = response.xpath("").extract_first()
+        return language
+
+    def parse_original_language(self, response):
+        # TODO I didn`t find this field yet
+        return None
+
+    def parse_paperback(self, response):
+        # TODO Implement
+        paperback = response.xpath("").extract_first()
+        return paperback
+
+    def parse_product_dimensions(self, response):
+        # TODO Implement
+        product_dimensions = response.xpath("").extract_first()
+        return product_dimensions
+
+    def parse_publisher(self, response):
+        # TODO Implement
+        publisher = response.xpath("").extract_first()
+        return publisher
+
+    def parse_publishing_year(self, response):
+        # TODO Implement
+        publishing_year = response.xpath("").extract_first()
+        return publishing_year
+
+    def parse_isbn(self, response):
+        # TODO Implement
+        isbn = response.xpath("").extract_first()
+        return isbn
+
+    def parse_image_urls(self, response):
+        # TODO Implement
+        image_url = response.xpath("").extract_first()
+        image_url = response.urljoin(image_url)
+        image_urls = [image_url]
+        return image_urls
+
+    def parse_weight(self, response):
+        # TODO Implement
+        weight = response.xpath("").extract_first()
+        return weight
---
+++ parser_src/spiders/knigosklad_spider.py
@@ -0,0 +1,36 @@
+from parsers.yakabooua_parser import YakaboouaParser
+# from parser_src.parsers.yakabooua_parser import YakaboouaParser
+from spiders.base_spider import BaseSpider
+
+
+class KnigoskladSpider(BaseSpider):
+
+    name = "knigosklad.com.ua"
+    allowed_domains = ["knigosklad.com.ua"]
+    book_url = None
+    category_id = None
+    custom_settings = {
+        'LOG_FILE': 'logs/knigosklad.txt',
+    }
+
+    def reparse_book(self, response):
+        knigosklad_parser = KnigoskladParser()
+        yield knigosklad_parser.reparse_book_page(response)
+
+    def get_number_of_pages_in_category(self, response) -> int:
+        number_of_pages = response.xpath("//div[contains(@class, 'pages')]/ol/li[last()-1]/a/text()").extract_first()
+        if number_of_pages is None:
+            return 1
+        else:
+            return int(number_of_pages)
+
+    def generate_urls(self, number_of_pages_in_category):
+        return (self.start_url + "?p=" + str(i) for i in range(1, number_of_pages_in_category + 1))
+
+    def get_pagination_items(self, response):
+        # TODO Maybe replace this method to Parser class
+        return response.xpath("//a[@class='product-image']/@href").extract()
+
+    def parse_pagination(self, response):
+        yakabooua_parser = YakaboouaParser()
+        yield yakabooua_parser.parse_book_page(response)
